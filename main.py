6c59dbc701a067e9da2accb615fddaa5581c345f
Neumi <Neumi>
2019-10-08 22:36:38 +0200

    added encoding from string to GSM7 encoding + cleanup

--- main.py
+++ main.py
@@ -1,64 +1,94 @@
 import requests
 from datetime import datetime
+import binascii
 
-now = datetime.now()
+
 phoneNumber = '0000000000' # enter phone number here
-message = '0074006500730074' # TODO message encoding rawMessage = 'test'
+
+message = 'Hello, world!'
 
 
-print(message)
+gsm = ("@£$¥èéùìòÇ\nØø\rÅåΔ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ !\"#¤%&'()*+,-./0123456789:;<=>?"
+       "¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ`¿abcdefghijklmnopqrstuvwxyzäöñüà")
+ext = ("````````````````````^```````````````````{}`````\\````````````[~]`"
+       "|````````````````````````````````````€``````````````````````````")
 
-smsTime = now.strftime("%y;%m;%d;%H;%M;%S;+2")
+def get_sms_time():
+    return datetime.now().strftime("%y;%m;%d;%H;%M;%S;+2")
 
-cookies = {
-    'stok': '',
-}
+def gsm_encode(plaintext):
+    res = bytearray()
+    for c in plaintext:
+        res.append(0)
+        idx = gsm.find(c)
+        if idx != -1:
+            res.append(idx)
+            continue
+        idx = ext.find(c)
+        if idx != -1:
+            res.append(27)
+            res.append(idx)
+    return binascii.hexlify(res)
 
-headers = {
-    'Origin': 'http://192.168.0.1',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
-    'Referer': 'http://192.168.0.1/home.htm',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Connection': 'keep-alive',
-}
+def get_cookie():
+    cookies = {
+        'stok': '',
+    }
 
-data = {
-  'password': 'YWRtaW4=', # equal to 'admin' as password in router
-  'goformId': 'LOGIN_EXCLUSIVE'
-}
-response = requests.post('http://192.168.0.1/goform/goform_set_cmd_process', headers=headers, cookies=cookies, data=data, verify=False)
+    headers = {
+        'Origin': 'http://192.168.0.1',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Referer': 'http://192.168.0.1/home.htm',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Connection': 'keep-alive',
+    }
 
-# get a admin login cookie, to emulate an admin user accessing web interface
-validCookie = str(response.cookies).replace('<RequestsCookieJar[<Cookie stok=', '').replace(' for 192.168.0.1/>]>', '')
+    data = {
+        'password': 'YWRtaW4=',  # equal to 'admin' as password in router
+        'goformId': 'LOGIN_EXCLUSIVE'
+    }
+    response = requests.post('http://192.168.0.1/goform/goform_set_cmd_process', headers=headers, cookies=cookies,
+                             data=data, verify=False)
 
-cookies = {
-    'stok': validCookie,
-}
+    return str(response.cookies).replace('<RequestsCookieJar[<Cookie stok=', '').replace(' for 192.168.0.1/>]>', '')
 
-headers = {
-    'Origin': 'http://vodafonemobile.wifi',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Referer': 'http://vodafonemobile.wifi/home.htm',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Connection': 'keep-alive',
-}
 
-data = {
-  'goformId': 'SEND_SMS',
-  'Number': phoneNumber,
-  'sms_time': smsTime,
-  'MessageBody': message,
-  'ID': '-1',
-  'encode_type': 'GSM7_default',
-}
 
+
+def send_sms(phoneNumber, messageEncoded):
+    cookies = {
+        'stok': get_cookie(),
+    }
+
+    headers = {
+        'Origin': 'http://vodafonemobile.wifi',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Referer': 'http://vodafonemobile.wifi/home.htm',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Connection': 'keep-alive',
+    }
+
+    data = {
+        'goformId': 'SEND_SMS',
+        'Number': phoneNumber,
+        'sms_time': get_sms_time(),
+        'MessageBody': messageEncoded,
+        'ID': '-1',
+        'encode_type': 'GSM7_default',
+    }
+
+    print('sending sms...')
+    response = requests.post('http://vodafonemobile.wifi/goform/goform_set_cmd_process', headers=headers,
+                             cookies=cookies, data=data, verify=False)
+    return response.content
+
+
+messageEncoded = gsm_encode(message)
 
 # send SMS
-print('sending sms...')
-response = requests.post('http://vodafonemobile.wifi/goform/goform_set_cmd_process', headers=headers, cookies=cookies, data=data, verify=False)
-print(response.content)
+print(send_sms(phoneNumber, messageEncoded))
